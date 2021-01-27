30dfa2939509ad4b60125a550120e1303f981fbd
WillL13 <78096737+WillL13@users.noreply.github.com>
2021-01-27 16:12:56 +0000

    Add files via upload

---
+++ client.lua
@@ -0,0 +1,16 @@
+function notify(string)
+    SetNotificationTextEntry("STRING")
+    AddTextComponentString(string)
+    DrawNotification(true, false)
+  end
+
+RegisterCommand('coords', function(source, args)
+local ped = PlayerPedId()
+local playercoords = GetEntityCoords(ped)
+notify('~r~X: '..playercoords.x)
+notify('~g~Y: '..playercoords.y)
+notify('~b~Z: '..playercoords.z)
+print('X: '..playercoords.x)
+print('Y: '..playercoords.y)
+print('Z: '..playercoords.z)
+end, false)
---
+++ fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version 'bodacious'
+games {'gta5' }
+
+author 'Sniped'
+description '/coords Gives Player Coords'
+version '1.0.0'
+
+client_script '/client/client.lua'
